30a2fbcff01f71e07c6d4a9cb682395a718e19e4
Jillian Martin <jillmartin@live.com>
2022-12-16 20:29:48 -0800

    add webpack prod config

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,7 +1,6 @@
 const commonConfig = require("./webpack.common");
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const packageJson = require('../package.json');
 
 const devConfig = {
@@ -20,9 +19,6 @@ const devConfig = {
       },
       shared: packageJson.dependencies,
     }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html'
-    })
   ]
 };
 
