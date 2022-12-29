59f5a2e97cb5cd9aecbae8aaec766ec8289ed6d2
Jillian Martin <jillmartin@live.com>
2022-12-28 19:56:53 -0800

    add some dummy products

--- packages/discovery/src/components/ProductListing.js
+++ packages/discovery/src/components/ProductListing.js
@@ -1,8 +1,36 @@
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import React from "react";
+
+const products = [{
+  name: "Pencil",
+  price: 1,
+}, {
+  name: "Pen",
+  price: 2,
+}, {
+  name: "Ruler",
+  price: 3,
+}];
 
 export const ProductListing = () => {
   return (
-    <div>I'm the product listing page</div>
+    <Grid container spacing={2}>
+      {products.map((product, index) => (
+        <Grid item key={index}>
+            <Card>
+              <Typography>
+                {product.name}
+              </Typography>
+              <Button>
+                Add to Cart
+              </Button>
+            </Card>
+        </Grid>
+      ))}
+    </Grid>
   )
 };
 
